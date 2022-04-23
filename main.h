1ef7409f3ee4df4d3532a4382936e40dda360962
dib-ops <fayemathias0@gmail.com>
2022-04-23 02:33:24 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,28 @@
-
 #ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap *f);
+} ph;
+
+/* prototypes */
+int _printf(const char *format, ...);
+
+#endif
+
+
+/** #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -13,15 +36,16 @@
  * @f: The function associated
  */
 
-typedef struct specifiers
+/**typedef struct specifiers
 {
 	char specifiers;
 	int (*f)(va_list);
 } specifiers_t;
 
 /*prototypes*/
-int _printf(const char *format, ...);
+/**int _printf(const char *format, ...);
 int get_function(char s, va_list args);
 int _putchar(char c);
 
 #endif
+*/
