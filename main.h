26bcee7e4abfe6b25d5b6b2865e510968994efa0
dib-ops <fayemathias0@gmail.com>
2022-04-23 02:40:03 +0000

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,20 @@
 #include <stdarg.h>
 
 /**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
  * @c: format specifier
@@ -13,10 +27,10 @@
 typedef struct printHandler
 {
 	char c;
-	int (*f)(va_list ap *f);
+	int (*f)(va_list ap, flags_t *f);
 } ph;
 
-/* prototypes */
+/* _printf */
 int _printf(const char *format, ...);
 
 #endif
