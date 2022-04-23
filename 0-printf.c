f6e445b8c5cf1e7a23a6eebeab77df1a43b5fa4b
Mairam-dev <diey333@gmail.com>
2022-04-23 01:25:55 +0000

    update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,47 +1,52 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 #include "main.h"
-#include <unistd.h>
 /**
- * _printf - Emulate the original.
- *
- * @format: Format by specifier.
- *
- * Return: count of chars.
+ * _printf - printf copu
+ * @format: format receive
+ * Return: ret
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0, count_fun;
-	va_list args;
-
-	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	va_list p;
+	int i = 0, j = 0, ret = 0, fret = 0, flag = 0;
+	pr pf_s[] = {
+		{'s', print_string}, {'c', print_char}, {'%', print_percent},
+		{'d', print_int}, {'i', print_int},
+		{'\0', NULL}};
+	va_start(p, format);
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	while (format[i])
-	{
-		count_fun = 0;
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{	flag = 0;
 		if (format[i] == '%')
 		{
-			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
-			{
-				count = -1;
-				break;
-			}
-			count_fun += get_function(format[i + 1], args);
-			if (count_fun == 0)
-				count += _putchar(format[i + 1]);
-			if (count_fun == -1)
-				count = -1;
+			if (format[i + 1] == '\0')
+				return (-1);
 			i++;
+			for (j = 0; pf_s[j].c != '\0'; j++)
+				if (format[i] == pf_s[j].c)
+				{
+					fret += pf_s[j].f(p);
+					flag = 1;
+				}
 		}
 		else
 		{
-			(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
+			_putchar(format[i]);
+			ret++;
+			flag = 1;
 		}
-		i++;
-		if (count != -1)
-			count += count_fun;
+		if (flag == 0)
+		{
+			i--;
+			_putchar(37);
+			fret++;
+		}
 	}
-	va_end(args);
-	return (count);
+	ret += fret;
+	return (ret);
 }
