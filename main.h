653ba429e151b4d34f137cb503ce818832334410
Mairam-dev <diey333@gmail.com>
2022-04-23 00:36:14 +0000

    add header file main.h

---
+++ main.h
@@ -0,0 +1,27 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list);
+} specifiers_t;
+
+/*prototypes*/
+int _printf(const char *format, ...);
+int get_function(char s, va_list args);
+int _putchar(char c);
+
+#endif
