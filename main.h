3b71f49eac95c4794cc86384b09974b6d6001067
dib-ops <fayemathias0@gmail.com>
2022-04-23 05:33:57 +0000

    Update main.h

--- main.h
+++ main.h
@@ -12,27 +12,12 @@
  * @f: The function associated
  */
 
-typedef struct specifiers
+typedef struct specifier
 {
 	char *specifiers;
 	int (*f)(va_list);
-} specifiers_t;
+} spec;
 
-int parser(const char *format, specifiers_t f_list[], va_list arg_list);
-int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
 /*prototypes*/
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
