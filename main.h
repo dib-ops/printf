daad238d15e1553a62167dcc2d3fe6563971c444
dib-ops <fayemathias0@gmail.com>
2022-04-23 02:58:18 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,44 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-
-/* _printf */
-int _printf(const char *format, ...);
-
-#endif
-
-
-/** #ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -50,16 +12,15 @@ int _printf(const char *format, ...);
  * @f: The function associated
  */
 
-/**typedef struct specifiers
+typedef struct specifiers
 {
-	char specifiers;
+	char *specifier;
 	int (*f)(va_list);
 } specifiers_t;
 
 /*prototypes*/
-/**int _printf(const char *format, ...);
+int _printf(const char *format, ...);
 int get_function(char s, va_list args);
 int _putchar(char c);
 
 #endif
-*/
